13a56f19a04c044d866fd889f01ed9070ca64956
Marco Antonio <roam130602@gmail.com>
2026-02-28 17:01:59 -0600

    Initial commit

---
+++ src/main/java/com/example/foro/ForoWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.foro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoWebApplication.class, args);
+	}
+
+}
